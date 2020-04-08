c00e0c656749ba799b60c047802175de893fccc0
Taufiquzzaman Peyash <taufiquzzaman.peyash@northsouth.edu>
2020-04-08 16:24:04 +0600

    changed model definition for new dropouts

--- model.py
+++ model.py
@@ -53,15 +53,15 @@ class RNNModel(nn.Module):
 class AdaptiveSoftmaxRNN(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
 
-    def __init__(self, ntoken, ninp, nhid, nlayers, dropout=0.5, rnn_dropout=0.2, cutoffs=[20000, 50000], tie_weights=True):
+    def __init__(self, ntoken, ninp, nhid, nlayers, emb_dropout=0.0, rnn_dropout=0.2, tail_droput=0.5, cutoffs=[20000, 50000], tie_weights=True):
         super(AdaptiveSoftmaxRNN, self).__init__()
         ntoken = ntoken
-        self.drop = nn.Dropout(dropout)
+        self.emb_dropout = nn.Dropout(emb_dropout)
         # self.encoder = nn.Embedding(ntoken, ninp)
-        self.encoder = AdaptiveInput(ninp, ntoken, cutoffs)
+        self.encoder = AdaptiveInput(ninp, ntoken, cutoffs, tail_drop=tail_dropout)
         self.rnn = nn.LSTM(ninp, nhid, nlayers, dropout=rnn_dropout)
         # self.decoder = nn.Linear(nhid, ntoken)
-        self.decoder = AdaptiveLogSoftmaxWithLoss(nhid, ntoken, cutoffs=cutoffs, div_value=2.0)
+        self.decoder = AdaptiveLogSoftmaxWithLoss(nhid, ntoken, cutoffs=cutoffs, div_value=2.0, tail_drop=tail_dropout)
         self.init_weights()
         self.nhid = nhid
         self.nlayers = nlayers
@@ -81,7 +81,7 @@ class AdaptiveSoftmaxRNN(nn.Module):
         # self.decoder.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, input, hidden, targets):
-        emb = self.encoder(input) # (seq_len, bsz, ninp)
+        emb = self.emb_dropout(self.encoder(input)) # (seq_len, bsz, ninp)
         output, hidden = self.rnn(emb, hidden) # (seq_len, bsz, ninp)
         output = self.drop(output)
         output = output.view(-1,output.size(2)) # (seq_len*bsz, ninp)
