1862f296a36eea43f52670e40f78212b9ab37b54
Taufiquzzaman Peyash <taufiquzzaman.peyash@northsouth.edu>
2020-05-01 13:23:39 +0600

    introduced vocab based on minimum frequency

--- data.py
+++ data.py
@@ -4,9 +4,11 @@ import torch
 from collections import Counter
 
 class Dictionary(object):
-    def __init__(self):
+    def __init__(self, min_freq=2, add_eos=False):
         self.total_tokens = 0
         self.counter = Counter()
+        self.min_freq = min_freq
+        self.add_eos = add_eos
 
     def add_word(self, word):
         if word not in self.word2idx:
@@ -14,27 +16,43 @@ class Dictionary(object):
             self.word2idx[word] = len(self.idx2word) - 1
         return self.word2idx[word]
     
-    def build_vocab(self, path, add_eos=False):
+    def build_vocab(self, path):
         with open(path, 'r', encoding="utf8") as f:
             for line in f:
-                if add_eos:
+                if self.add_eos:
                     words = line.split() + ['<eos>']
                 else:
                     words = line.split()
 
-                # for word in words:
-                #     self.add_word(word)
                 self.counter.update(words)
                 self.total_tokens += len(words)
     
     def sort_vocab(self):
-        self.word2idx = {}
-        self.idx2word = []
+        self.word2idx = {'<unk>': 0}
+        self.idx2word = ['<unk>']
 
-        for word, _ in self.counter.most_common():
-            self.idx2word.append(word)
-            self.word2idx[word] = len(self.idx2word) - 1
+        if self.add_eos:
+            self.word2idx = {'<eos>': 1}
+            self.idx2word.append('<eos>')
 
+        for word, frequency in self.counter.most_common():
+            if frequency >= self.min_freq:
+                self.idx2word.append(word)
+                self.word2idx[word] = len(self.idx2word) - 1
+    
+    def count_from_freq(self, files, min_freq):
+          dictionary = Counter()
+          for filename in files:
+              with open(filename, 'r', encoding='utf8') as f:
+                  for line in f:
+                      dictionary.update(line.split())
+
+          count = 0
+          for _, freq in dictionary.most_common():
+              if freq >= min_freq:
+                  count += 1
+          
+          return count
 
     def __len__(self):
         return len(self.idx2word)
