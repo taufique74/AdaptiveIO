30f8b5793d4b2a39d48e894b9f7f68d657a50af7
taufique74 <taufiquzzaman.peyash@northsouth.edu>
2020-05-03 12:49:27 +0600

    dictionary takes vocab cache

--- data.py
+++ data.py
@@ -1,14 +1,16 @@
 import os
 from io import open
 import torch
+import pickle
 from collections import Counter
 
 class Dictionary(object):
-    def __init__(self, min_freq=2, add_eos=False):
+    def __init__(self, vocab_cache, min_freq=2, add_eos=False):
         self.total_tokens = 0
         self.counter = Counter()
         self.min_freq = min_freq
         self.add_eos = add_eos
+        self.vocab_cache = vocab_cache
 
     def add_word(self, word):
         if word not in self.word2idx:
@@ -41,34 +43,43 @@ class Dictionary(object):
                 self.word2idx[word] = len(self.idx2word) - 1
     
     def count_from_freq(self, files, min_freq):
-          dictionary = Counter()
-          for filename in files:
-              with open(filename, 'r', encoding='utf8') as f:
-                  for line in f:
-                      dictionary.update(line.split())
+        dictionary = Counter()
+        for filename in files:
+            with open(filename, 'r', encoding='utf8') as f:
+                for line in f:
+                    dictionary.update(line.split())
 
-          count = 0
-          for _, freq in dictionary.most_common():
-              if freq >= min_freq:
-                  count += 1
+        count = 0
+        for _, freq in dictionary.most_common():
+            if freq >= min_freq:
+                count += 1
           
-          return count
+        return count
+    
+    def load_from_cache(self):
+        self.idx2word = self.vocab_cache['idx2word']
+        self.word2idx = self.vocab_cache['word2idx']
+        self.total_tokens = self.vocab_cache['total_tokens']
+    
 
     def __len__(self):
         return len(self.idx2word)
 
 class Corpus(object):
-    def __init__(self, path, min_freq=2, add_eos=False):
-        self.dictionary = Dictionary(min_freq, add_eos)
+    def __init__(self, path, min_freq=2, add_eos=False, vocab_cache=None):
+        self.dictionary = Dictionary(vocab_cache, min_freq, add_eos)
         train_path = os.path.join(path, 'train.txt')
         test_path = os.path.join(path, 'test.txt')
         valid_path = os.path.join(path, 'valid.txt')
-
-        self.dictionary.build_vocab(train_path)
-        self.dictionary.build_vocab(test_path)
-        self.dictionary.build_vocab(valid_path)
         
-        self.dictionary.sort_vocab()
+        if vocab_cache is not None:
+            self.dictionary.load_from_cache()
+        else:
+            self.dictionary.build_vocab(train_path)
+            self.dictionary.build_vocab(test_path)
+            self.dictionary.build_vocab(valid_path)
+
+            self.dictionary.sort_vocab()
         
         self.train = self.tokenize(train_path)
         self.valid = self.tokenize(valid_path)
